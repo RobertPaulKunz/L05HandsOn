88b8ad901c1e2d260ac4e9dd73f4ba03a2bb4785
Robert Kunz <robertpkunz@hotmail.com>
2020-04-23 18:04:08 -0400

    added links to index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-alert("it's alive");
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Link } from 'react-router-dom';
+
+function App() {
+    return (
+        <Router>
+            <div>
+                <ul>
+                    <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
+                    <Link to='/about'>About</Link>
+                    </li>
+                    <li>
+                    <Link to='/favoritethings'>Favorite Things</Link>
+                    </li>
+                </ul>
+            </div>
+        </Router>
+    )
+}
+
+const rootElement = document.getElementById('root');
+ReactDOM.render(<App />, rootElement);
