402d64bb83b38e9945130cb88173a75a6a5c057b
Robert Kunz <robertpkunz@hotmail.com>
2020-04-23 20:02:57 -0400

    not sure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import About from './components/About';
+import Home from './components/Home';
+import FavoriteThings from './components/FavoriteThings';
+import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
 
 function App() {
     return (
@@ -11,14 +14,17 @@ function App() {
                         <Link to='/'>Home</Link>
                     </li>
                     <li>
-                    <Link to='/about'>About</Link>
+                        <Link to='/about'>About</Link>
                     </li>
                     <li>
-                    <Link to='/favoritethings'>Favorite Things</Link>
+                        <Link to='/favoritethings'>Favorite Things</Link>
                     </li>
                 </ul>
+                <hr />
 
-            <hr />
+                <Route exact path='/' component={Home} />
+                <Route path='/about' component ={About} />
+                <Route path='/favoritethings' component={FavoriteThings} />
 
             </div>
         </Router>
